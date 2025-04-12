24af58b96217fef72dc06eedaafb69692205f283
Alexandre Hardy <alexandre.hardy@gmail.com>
2025-04-12 20:30:45 +0200

    Add missing file

---
+++ src/lib/blockly/genid.ts
@@ -0,0 +1,10 @@
+export function genId() {
+    const soup = '_-ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const length = 12;
+    const soupLength = soup.length;
+    const id = [];
+    for (let i = 0; i < length; i++) {
+        id[i] = soup.charAt(Math.random() * soupLength);
+    }
+    return id.join('');
+}
